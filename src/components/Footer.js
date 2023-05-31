fe86c4e1ff38b9b8977ce707427fb25eb54611ba
Ajit Dhdharia <ajitdhdhariaaa@gmail.com>
2023-05-31 09:31:38 -0400

    add header and footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,117 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <>
-    
+      <footer className="footer-newsletter py-3 px-3">
+        <div className="container-xxl">
+          <div className="row align-items-center mb-0">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-itmes-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h3 className="mb-0 text-white">Sign Up for Newsletter</h3>
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="input-group">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Your Email address"
+                  aria-label="Your Email address"
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-2" id="basic-addon2">
+                  Subscribe
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-3 px-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h5 className="text-white mb-2">Contact Us</h5>
+              <div className="text-white">
+                <address className="mb-1">
+                  33-10 Esterbroke Avenue, North York, Ontario, Canada - M2J 2C2
+                </address>
+                <a
+                  href="tel: +1 437-553-1005"
+                  className="text-white mt-1 d-block mb-1"
+                >
+                  +1 437-553-1005
+                </a>
+                <a
+                  href="mailto:ajitdhdhariaaa@gmail.com"
+                  className="text-white mt-1 d-block mb-4"
+                >
+                  ajitdhdhariaaa@gmail.com
+                </a>
+                <div className="social_icons d-flex align-items-center gap-15">
+                  <a className="text-white" href="">
+                    <BsLinkedin />
+                  </a>
+                  <a className="text-white" href="">
+                    <BsInstagram />
+                  </a>
+                  <a className="text-white" href="">
+                    <BsGithub />
+                  </a>
+                  <a className="text-white" href="">
+                    <BsYoutube />
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <h5 className="text-white mb-2">Information</h5>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-1">Privacy Policy</Link>
+                <Link className="text-white py-1">Refund Policy</Link>
+                <Link className="text-white py-1">Shipping Policy</Link>
+                <Link className="text-white py-1">Terms & Conditions</Link>
+                <Link className="text-white py-1">Blogs</Link>
+              </div>
+            </div>
+            <div className="col-3">
+              <h5 className="text-white mb-2">Account</h5>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-1">About Us</Link>
+                <Link className="text-white py-1">FAQs</Link>
+                <Link className="text-white py-1">Contact</Link>
+              </div>
+            </div>
+            <div className="col-2">
+              <h5 className="text-white mb-2">Quick Links</h5>
+              <div className="footer-links d-flex flex-column">
+                <Link className="text-white py-1">Laptops</Link>
+                <Link className="text-white py-1">Headphones</Link>
+                <Link className="text-white py-1">Tablet</Link>
+                <Link className="text-white py-1">Watch</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-3 px-3">
+        <div className="contianer-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                {" "}
+                &copy; {new Date().getFullYear()}; Powered by Indie Couture
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import { BsSearch } from "react-icons/bs";
 const Header = () => {
   return (
     <>
-      <header className="header-top-strip py-1 px-3">
+      <header className="header-top-strip py-2 px-3">
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
@@ -24,19 +24,19 @@ const Header = () => {
           </div>
         </div>
       </header>
-      <header className="header-upper py-1">
+      <header className="header-upper my-0 py-2 px-3">
         <div className="container-xxl">
-          <div className="row">
-            <div className="col-2">
-              <h2 className="text-white">Indie Couture</h2>
+          <div className="row align-items-center">
+            <div className="col-3">
+              <h3 className="text-white mb-0">INDIE COUTURE</h3>
             </div>
             <div className="col-5">
-              <div className="input-group mb-3">
-                 <input
+              <div className="input-group">
+                <input
                   type="text"
                   className="form-control"
-                  placeholder="Recipient's username"
-                  aria-label="Recipient's username"
+                  placeholder="Search items"
+                  aria-label="Search items"
                   aria-describedby="basic-addon2"
                 />
                 <span className="input-group-text" id="basic-addon2">
@@ -44,94 +44,80 @@ const Header = () => {
                 </span>
               </div>
             </div>
-            <div className="col-5">
-              <div className="header-upper-links d-flex aligm-itmes-center justify-content-between">
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
-                    <img src="images/compare.svg" alt="compare" />
-                    <p>
-                      Compare <br /> Products
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
-                    <img src="images/wishlist.svg" alt="wishlist" />
-                    <p>
-                      Favorite <br /> Wishlist
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
-                    <img src="images/user.svg" alt="user" />
-                    <p>
-                      Log In <br /> My Account
-                    </p>
-                  </Link>
-                </div>
-                <div>
-                  <Link className="d-flex align-items-center gap-10 text-white">
-                    <img src="images/cart.svg" alt="cart" />
-                    <div className="d-flex felx-column gap-10">
-                      <span className="badge bg-white text-dark">0</span>
-                      <p>$ 500</p>
-                    </div>
-                  </Link>
-                </div>
+            <div className="col-4">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <Link className="d-flex align-items-center gap-10 text-white">
+                  <img src="images/compare.svg" alt="compare" />
+                  <p className="mb-0">
+                    Compare <br /> Products
+                  </p>
+                </Link>
+                <Link className="d-flex align-items-center gap-10 text-white">
+                  <img src="images/wishlist.svg" alt="wishlist" />
+                  <p className="mb-0">
+                    Favorite <br /> Wishlist
+                  </p>
+                </Link>
+                <Link className="d-flex align-items-center gap-10 text-white">
+                  <img src="images/user.svg" alt="user" />
+                  <p className="mb-0">
+                    Log In <br /> My Account
+                  </p>
+                </Link>
+                <Link className="d-flex align-items-center gap-10 text-white">
+                  <img src="images/cart.svg" alt="cart" />
+                  <div className="d-flex felx-column gap-10">
+                    <span className="badge bg-white text-dark">0</span>
+                    <p className="mb-0">$ 500</p>
+                  </div>
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </header>
-      <header className="header-bottom">
+      <header className="header-bottom mb-0">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-12">
-              <div className="menu-bottom d-flex align-items-center">
-                <div>
-                  <div className="dropdown">
-                    <button
-                      className="btn btn-secondary dropdown-toggle bg-transparent border-0 gap-15 d-flex" 
-                      type="button"
-                      id="dropdownMenuButton1"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      <img src="images/menu.svg" alt="" />
-                      <span>Shop Categories</span>
-                    </button>
-                    <ul
-                      className="dropdown-menu"
-                      aria-labelledby="dropdownMenuButton1"
-                    >
-                      <li>
-                        <Link className="dropdown-item" to=''>
-                          Action
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="dropdown-item" to=''>
-                          Another action
-                        </Link>
-                      </li>
-                      <li>
-                        <Link className="dropdown-item" to=''>
-                          Something else here
-                        </Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="menu-links">
-                  <div className="d-flex align-items-center gap-15">
-                    <NavLink to="/">Home</NavLink>
-                    <NavLink to="/">Our Store</NavLink>
-                    <NavLink to="/">Blogs</NavLink>
-                    <NavLink to="/">Contact</NavLink>
-                  </div>
-                </div>
+            <div className="col-3 menu-bottom d-flex align-items-center">
+              <div className="dropdown pl-1">
+                <button
+                  className="btn btn-secondary dropdown-toggle bg-transparent border-0"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img src="images/menu.svg" alt="menu"/>
+                  <span className="me-5 d-inline-block px-2">Shop Categories</span>
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li>
+                    <Link className="dropdown-item" to="">
+                      Action
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="">
+                      Another action
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="dropdown-item" to="">
+                      Something else here
+                    </Link>
+                  </li>
+                </ul>
               </div>
+            </div>
+            <div className="col-9 menu-links d-flex align-items-center gap-30 pl-4">
+              <NavLink to="/">Home</NavLink>
+              <NavLink to="/">Our Store</NavLink>
+              <NavLink to="/">Blogs</NavLink>
+              <NavLink to="/">Contact</NavLink>
             </div>
           </div>
         </div>
